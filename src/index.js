f53d15817fbc3ed28f857e670beafc7473f83c21
zvecr <git@zvecr.com>
2019-06-02 22:17:17 +0100

    Tidy up cli slightly

--- src/index.js
+++ src/index.js
@@ -5,25 +5,19 @@ class BootloadHIDCli extends Command {
   async run() {
     const { args, flags: flagz } = this.parse(BootloadHIDCli);
 
-    // console.log("CLI args:", flagz, args);
-
-    BootloadHID.list((err, ports) => {
-      console.log('devices', ports);
-    });
-
     const loader = new BootloadHID({
       debug: false,
       manualReset: !flagz.leaveBootLoader,
     });
 
-    loader.on('flash:start', (info, startAddr, endAddr, pageSize, deviceSize) => {
+    loader.on('flash:start', (dev, startAddr, endAddr, pageSize, deviceSize) => {
       const totalData = endAddr - startAddr;
 
       console.log(`Page size   = ${pageSize} (0x${pageSize.toString(16)})`);
       console.log(`Device size = ${deviceSize} (0x${deviceSize.toString(16)}); ${deviceSize - 2048} bytes remaining`);
       console.log(`Uploading ${totalData} (0x${totalData.toString(16)}) bytes starting at ${startAddr} (0x${startAddr.toString(16)})`);
     });
-    loader.on('flash:progress', (info, startAddr, endAddr) => {
+    loader.on('flash:progress', (dev, startAddr, endAddr) => {
       console.log(`0x${startAddr.toString(16)} ... 0x${endAddr.toString(16)}`);
     });
 
@@ -31,8 +25,6 @@ class BootloadHIDCli extends Command {
     loader.flash(args.file, (error) => {
       if (error) {
         console.error(error.message);
-      } else {
-        // console.info('done.');
       }
     });
   }
@@ -50,7 +42,7 @@ BootloadHIDCli.flags = {
   // add --help flag to show CLI version
   help: flags.help({ char: 'h' }),
 
-  // reboot flag
+  // app specific flags
   leaveBootLoader: flags.boolean({
     char: 'r',
     description: 'reboot device once flashing has finished',
